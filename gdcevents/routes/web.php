7167cbd98229049fdef9a4091a09a190fd8f07fd
Pancosta <dz6.gustav@gmail.com>
2023-04-07 13:28:28 -0300

    formulário de criar evento salvando no banco e exibindo na home

--- gdcevents/routes/web.php
+++ gdcevents/routes/web.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\ContactController;
 
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
+Route::post('/events', [EventController::class,'store']);
 
 Route::get('/contacts/contact', [ContactController::class, 'contactIndex']);
 
