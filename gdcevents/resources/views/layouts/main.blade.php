1e0f118929fa16ec46995e53084b8a74907c1b9b
Pancosta <dz6.gustav@gmail.com>
2023-05-01 12:52:53 -0300

    adicionando flash message de sucesso na home ao criar um evento

--- gdcevents/resources/views/layouts/main.blade.php
+++ gdcevents/resources/views/layouts/main.blade.php
@@ -1,46 +1,59 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>@yield('title')</title>
-        <!-- Fontes do Google -->
-        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
-        <!-- Css Bootstrap -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <!--Css Aplicação -->
-        <link rel="stylesheet" href="/css/styles.css"/>
-        <script src="/js/script.js"> </script>
-    </head>
-    <body>
-        <header>
-            <nav class="navbar navbar-expand-lg navbar-light" >
-                <div class="collapse navbar-collapse" id="navbar">
-                    <a href="/" class="navbar-brand">
-                        <img src="/img/gdcevents_logo.svg" alt="GDC Events">                 
-                    </a>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a href="/"class="nav-link">Eventos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/events/create"class="nav-link">Criar Eventos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/"class="nav-link">Entrar</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/contacts/contact"class="nav-link">Cadastrar</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-        @yield('content')
-        <footer>
-            <p>GDC Events &copy; 2023</p>
-        </footer>
-        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    <!-- Fontes do Google -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+    <!-- Css Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!--Css Aplicação -->
+    <link rel="stylesheet" href="/css/styles.css" />
+    <script src="/js/script.js"> </script>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="collapse navbar-collapse" id="navbar">
+                <a href="/" class="navbar-brand">
+                    <img src="/img/gdcevents_logo.svg" alt="GDC Events">
+                </a>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Eventos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/events/create" class="nav-link">Criar Eventos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Entrar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/contacts/contact" class="nav-link">Cadastrar</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+                @if(session('msg'))
+                <p class="msg">{{session('msg')}}</p>
+                @endif
+                @yield('content')
+            </div>
+        </div>
+    </main>
+    <footer>
+        <p>GDC Events &copy; 2023</p>
+    </footer>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+
 </html>
